c9ecd1d458cd65772e7f9c4c40f69d0cd7b33bab
KusalDemo <kusaldemo2022@gmail.com>
2023-12-15 10:10:37 +0530

    Initial commit

---
+++ src/main/java/com/example/layeredarchitecture/dao/CustomerDAOImpl.java
@@ -0,0 +1,25 @@
+package com.example.layeredarchitecture.dao;
+
+import com.example.layeredarchitecture.db.DBConnection;
+import com.example.layeredarchitecture.model.CustomerDTO;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public class CustomerDAOImpl {
+    public ArrayList<CustomerDTO> getAllCustomers() throws SQLException, ClassNotFoundException {
+        Connection connection = DBConnection.getDbConnection().getConnection();
+        Statement stm = connection.createStatement();
+        ResultSet rst = stm.executeQuery("SELECT * FROM Customer");
+        ArrayList<CustomerDTO> getAllCustomers = new ArrayList<>();
+
+        while (rst.next()){
+            getAllCustomers.add(new CustomerDTO(rst.getString(1), rst.getString(2), rst.getString(3)));
+        }
+        return getAllCustomers;
+    }
+
+}
